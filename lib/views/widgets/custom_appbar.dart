93014eced179dc7ea0bd5bddcbeb705da8727724
sahar <saharma20021@gmail.com>
2023-12-15 11:34:50 +0200

    create custom search icon

--- lib/views/widgets/custom_appbar.dart
+++ lib/views/widgets/custom_appbar.dart
@@ -5,11 +5,33 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(children: [
-      Text('Notes', style: TextStyle(
-        fontSize: 28
-      ),)
-      
-    ],);
+    return const Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 28),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+} 
+
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.05),
+      ),
+      child:const Icon(Icons.search, size: 28,),
+    );
   }
 }
